5f2cdbc291b5217f25790a71d809d2927f55b415
abied11 <juangramadhanabid@gmail.com>
2025-05-09 13:17:39 +0700

    Menambahkan kegiatan

--- src/App.vue
+++ src/App.vue
@@ -12,54 +12,5 @@
       />
       <button class="add-btn" type="submit">🏅 Tambah</button>
     </form>
-
-    <!-- Daftar Kegiatan -->
-    <div v-if="todos.length === 0" class="empty-msg">Belum ada rencana olahraga hari ini 💤</div>
-    <ul class="list">
-      <li
-        v-for="(todo, index) in filteredTodos"
-        :key="index"
-        class="todo-item"
-        :class="{ done: todo.done }"
-      >
-        <label>
-          <input type="checkbox" v-model="todo.done" />
-          <span class="todo-text">{{ todo.text }}</span>
-        </label>
-        <button class="delete-btn" @click="deleteTodo(index)">❌</button>
-      </li>
-    </ul>
-
-    <!-- Tombol Filter -->
-    <button class="filter-btn" @click="toggleFilter">
-      {{ filter ? 'Tampilkan Semua Kegiatan' : 'Hanya Yang Belum Selesai' }}
-    </button>
-  </div>
-</template>
-
-<script setup>
-import { ref, computed } from 'vue'
-
-const todos = ref([])
-const text = ref('')
-const filter = ref(false)
-
-function submitTodo() {
-  if (text.value.trim()) {
-    todos.value.push({ text: text.value.trim(), done: false })
-    text.value = ''
-  }
-}
-
-function deleteTodo(index) {
-  todos.value.splice(index, 1)
-}
-
-function toggleFilter() {
-  filter.value = !filter.value
-}
-
-const filteredTodos = computed(() =>
-  filter.value ? todos.value.filter(todo => !todo.done) : todos.value
-)
-</script>
+    </div>
+  </template>
